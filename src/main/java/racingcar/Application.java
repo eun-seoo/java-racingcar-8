663a88ad5ca838b93098e6edf0b9c417bc481797
eun-seoo <kimeunseo040@gamil.com>
2025-10-27 19:50:34 +0900

    refactor(app): Application 구조 및 예외 처리 통합

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import java.util.*;
+import racingcar.domain.RacingGame;
+import racingcar.domain.Winner;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            String carNames = InputView.readCarNames();
+            List<String> carNameList = Arrays.asList(carNames.split(","));
+            int tryCount = InputView.readTryCount();
+
+            RacingGame racingGame = new RacingGame(carNameList, tryCount);
+            racingGame.start();
+
+            Winner winner = new Winner(racingGame.getCars());
+            OutputView.printWinners(winner.getWinners());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
